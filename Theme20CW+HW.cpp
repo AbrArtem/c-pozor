3bdc61416b3926d2a8c678af0a025d4cee06cbc6
AbrArtem <106632118+AbrArtem@users.noreply.github.com>
2022-06-03 00:31:08 +0300

    Add files via upload

---
+++ Theme20CW+HW.cpp
@@ -0,0 +1,49 @@
+﻿// prog20_1
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include <set>
+#include <map>
+
+int main() {
+    std::fstream in("text.txt");
+    std::string s;
+    std::map<std::string, unsigned> counter;
+
+    while (!in.eof()) {
+        in >> s;
+
+        ++counter[s];
+    }
+
+    for (const auto& [word, count] : counter)
+        std::cout << word << '\t' << count << '\n';
+
+    in.close();
+}
+// prog20_2
+#include <iostream>
+#include <iterator>
+#include <set>
+
+int main() {
+
+    std::set<double> nums = { 1, 2, 3, 4, 5, 6 };
+    std::set<double> tmp;
+
+    while (nums.size() != 1) {
+        for (auto it = nums.cbegin(); it != nums.cend(); it++) {
+            if (std::next(it) != nums.cend())
+                tmp.insert(*it + *std::next(it));
+        }
+
+        nums = tmp;
+
+        tmp.clear();
+    }
+
+    for (auto i : nums)
+        std::cout << i << '\n';
+}
